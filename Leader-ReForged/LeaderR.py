66212a928dbfbef9779d50b543d27178dbf2390a
Hail122 <75189571+linstar-fxt@users.noreply.github.com>
2021-08-13 10:59:53 +0800

    Fixed Bugs

--- Leader-ReForged/LeaderR.py
+++ Leader-ReForged/LeaderR.py
@@ -5,13 +5,12 @@ from mcdreforged.info import Info
 
 PLUGIN_METADATA = {
     'id': 'leaderreforged',
-    'version': '1.0.0',
+    'version': '1.0.1',
     'name': 'LeaderR',
     'author': 'linstar-fxt',
     'link': 'https://github.com/linstar-fxt/MyMCDRPlugins/tree/main/Leader-ReForged',
     'dependencies': {
-            'mcdreforged': '>=1.0.0',
-            'minecraft_data_api': '>= 1.3.0'
+            'minecraft_data_api': '>= 1.3.0',
     }
 }
 
@@ -38,33 +37,28 @@ def leader(src: CommandSource):
     LeaderStat = True
     Leader = src.player   #等待验证
     server = src.get_server()
-    api = server.get_plugin_instance('minecraft_data_api')
-    dim = api.get_player_dimension(Leader)
-    cord = api.get_player_coordinate(Leader)
-
     server.say('[LeaderR] 已标记玩家§b {} §r为引导者,快跟随他叭~'.format(Leader))
-    server.execute('effect give {} minecraft:glowing 9999 1'.format(Leader))
+    server.execute('effect give {} minecraft:glowing 60 1'.format(Leader))
     src.reply('§7[LeaderR] 你成为了引导者,每隔60秒将广播坐标')
     src.reply('§7[LeaderR] 使用!!unleader取消引领')
-    pos_Leader(server, dim, cord, Leader)
     while LeaderStat:
         time.sleep(60)
+        api = server.get_plugin_instance('minecraft_data_api')
         if not LeaderStat:
             break
         else:
-            src.reply('§7 [LeaderR]引领者每隔60秒广播坐标，使用!!unleader取消引领')
-            pos_Leader(server, dim, cord, Leader)
-    
-def pos_Leader(server, dim, cord, Leader):
-    if dim == 0:
-        server.say('§6 引领者{} §7@ {} [x:{}, y:{}, z:{}] -> §c下界§r [x:{}, y:{}, z:{}]'.format(Leader, dim_convert[dim], int(cord.x), int(cord.y), int(cord.z), int((cord.x)/8), int(cord.y), int((cord.z)/8)))
-    elif dim == -1:
-        server.say('§6 引领者{} §7@ {} [x:{}, y:{}, z:{}] -> §a主世界§r [x:{}, y:{}, z:{}]'.format(Leader, dim_convert[dim], int(cord.x), int(cord.y), int(cord.z), int((cord.x)*8), int(cord.y), int((cord.z)*8)))
-    elif dim == 1:
-        server.say('§6 引领者{} §7@ {} [x:{}, y:{}, z:{}]'.format(Leader, dim_convert[dim], int(cord.x), int(cord.y), int(cord.z)))
-    else:
-        return
-
+            try:
+                dim = api.get_player_dimension(Leader)
+                cord = api.get_player_coordinate(Leader)
+                server.say('引领者 §b{} §r @ {} [x:{}, y:{}, z:{}]'.format(Leader, dim_convert[dim], int(cord.x), int(cord.y), int(cord.z)))
+                server.execute('effect give {} minecraft:glowing 60 1'.format(Leader))
+                src.reply('§7 [LeaderR]引领者每隔60秒广播坐标，使用!!unleader取消引领')
+            except:
+                server.say('[LeaderR] 找不到引领者!')
+                LeaderStat = False
+                Leader = None
+                break
+   
 def unleader(src: CommandSource):
     global LeaderStat
     global Leader
@@ -78,8 +72,13 @@ def unleader(src: CommandSource):
     else:
         src.reply('§c你不是引领者!')
 
-def on_load(server: ServerInterface, prev):
+def on_load(server: ServerInterface, old):
+    global Leader
+    global LeaderStat
     server.register_help_message('!!leader', '成为一名引领者')         
     server.register_help_message('!!unleader', '放弃成为一名引领者')      
     server.register_command(Literal('!!leader').runs(leader))            
-    server.register_command(Literal('!!unleader').runs(unleader))           
+    server.register_command(Literal('!!unleader').runs(unleader))    
+    if old is not None:
+        Leader = old.Leader
+        LeaderStat = old.LeaderStat
